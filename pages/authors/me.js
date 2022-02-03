19f6a5cec3effa452d41839d3ce2816cc65476d0
Abas Djumadi <abasd04@gmail.com>
2022-02-03 14:27:10 +0800

    my blog

---
+++ pages/authors/me.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Me(){
+    return (
+        <>
+            <Head>
+                About me
+            </Head>
+            <h1>Tentang saya</h1>
+        </>
+    )
+}
